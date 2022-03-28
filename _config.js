704f54c4c75f899bb983d60c19fb3328e4976d1b
Abderrahmane <mr.abderrahmane@gmail.com>
2022-03-28 19:33:42 +0000

    navbar and index

--- _config.js
+++ _config.js
@@ -2,7 +2,8 @@ import lume from "lume/mod.ts";
 
 const site = lume();
 site.copy("js", "js")
-//site.copy("css", "css") 
+site.copy("css", "css") 
+site.copy("img", "img") 
 site.ignore("convert.txt","mydata","README.md");
 
 export default site;
